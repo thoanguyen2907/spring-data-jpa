0035a0ff7d6bc526767468e95a764798849b9f71
thoanguyen2907 <thoanguyen290795@gmail.com>
2022-07-24 22:56:27 +0300

    add repository

--- src/main/java/com/example/datajpa/DataJpaApplication.java
+++ src/main/java/com/example/datajpa/DataJpaApplication.java
@@ -1,13 +1,23 @@
 package com.example.datajpa;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DataJpaApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(DataJpaApplication.class, args);
 	}
-
+	@Bean
+	CommandLineRunner commandLineRunner(StudentRepository studentRepository){
+	return args ->  {
+			Student maria = new Student("maria", "Jones", "maria@gmail", 20);
+		     studentRepository.save(maria);
+			 
+	};
+	}
 }
