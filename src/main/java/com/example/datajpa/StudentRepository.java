dd514358655f29c1636de0c313e22ce451c187c0
thoanguyen2907 <thoanguyen290795@gmail.com>
2022-07-25 14:40:56 +0300

    add SQL query for selecting

--- src/main/java/com/example/datajpa/StudentRepository.java
+++ src/main/java/com/example/datajpa/StudentRepository.java
@@ -1,8 +1,27 @@
 package com.example.datajpa;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface StudentRepository extends JpaRepository <Student, Long> {
+    @Query("SELECT s FROM Student s WHERE s.email = ?1")
+    Optional<Student> findStudentByEmail(String email);
 
+    @Query("SELECT s FROM Student s WHERE s.firstName = ?1 and s.age >= ?2")
+    List<Student> findStudentByFirstNameEqualsAndAgeEquals(String firstNam, Integer age);
 
+    @Query(value = "SELECT * FROM student WHERE first_name = :firstName AND age >= :age", nativeQuery = true)
+    List<Student> selectStudentWhereFirstNameAndAgeGreaterOrEqualNative(
+            @Param("firstName") String firstName,
+            @Param("age") Integer age);
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM Student u where u.id = ?1")
+    int deleteStudentById(Long id);
 }
